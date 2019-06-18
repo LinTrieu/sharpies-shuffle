8066e288b9ce4a918b457ec66fab030bd6373dcd
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-06-18 11:07:06 +0100

    Creates shuffle feature on group

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main()
+        {
+           string[] TheSharpies = new string[] { "Megan", "Lauren", "Taj", "Ed", "Lin" };      
+           Random rand = new Random();
+           string[] ShuffledSharpies = TheSharpies.OrderBy(x => rand.Next(TheSharpies.Length)).ToArray();
+           Console.WriteLine(ShuffledSharpies);
+           for (int i=0; i<5; i++)
+            {
+            Console.WriteLine(ShuffledSharpies[i]);      
+            }         
+        }
+    }
+}
